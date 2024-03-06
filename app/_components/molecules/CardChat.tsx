c47961bb2b7f4c13ace5fe2d77bc32569cbea700
anggakusumap <kusuma_putra@student.unud.ac.id>
2024-03-07 02:38:44 +0800

    feat: chat

--- app/_components/molecules/CardChat.tsx
+++ app/_components/molecules/CardChat.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import styles from "../organisms/MainContent.module.css";
-import { messageList } from '@/constants';
+import { Avatar, messageList } from '@/constants';
 import ChatMessage from '../atoms/ChatMessage';
 import { FaRegSmile } from 'react-icons/fa';
 import { FaPaperclip } from 'react-icons/fa6';
 import Button from '../atoms/Button';
 
 const CardChat = () => {
+    const [ messages, setMessages ] = useState(messageList);
+    const [ newMessageText, setNewMessageText ] = useState('');
+
+    const handleSendMessage = () => {
+        if (newMessageText.trim() !== '')
+        {
+            const newMessage = {
+                id: messages.length + 1,
+                message: newMessageText.trim(),
+                img: Avatar,
+                type: 'sender',
+            };
+
+            setMessages([ ...messages, newMessage ]);
+            setNewMessageText('');
+        }
+    };
+
     return (
         <div className={ styles.cardChat }>
             <h3>Esther Howard</h3>
             <div className={ styles.chatContainer }>
-                { messageList.map((list) => (
+                { messages.map((list) => (
                     <ChatMessage
                         key={ list.id }
                         img={ list.img }
@@ -21,13 +41,20 @@ const CardChat = () => {
                 )) }
             </div>
             <div className={ styles.chatInput }>
-                <input type="text" name="chatInput" id="chatInput" placeholder='Type your message here' />
+                <input
+                    type="text"
+                    name="chatInput"
+                    id="chatInput"
+                    value={ newMessageText }
+                    onChange={ (e) => setNewMessageText(e.target.value) }
+                    placeholder="Type your message here"
+                />
                 <div className={ styles.bottomInput }>
                     <div>
                         <FaRegSmile />
                         <FaPaperclip />
                     </div>
-                    <Button className={ styles.buttonSend } >Send now</Button>
+                    <Button className={ styles.buttonSend } onClick={ handleSendMessage }>Send now</Button>
                 </div>
             </div>
         </div>
